96befbe07aeecd0f42d64e9b3e70d05ac98036c9
giuliobrugnaro <giuliobrugnaro@gmail.com>
2022-11-12 22:20:12 +0100

    change filename to solve streamlit deployment issue

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import main
+import Main
 import streamlit as st
 
 st.set_page_config(page_title="OpenAI Hackathon - Climate Policy Copilot", page_icon="random", layout="wide")
-main.app()
+Main.app()
