d11c92ecfe6f8193baa363b471c6ab3b91850283
tommitassi <tommytassi@hotmail.it>
2022-11-13 20:12:29 +0100

    Save all outputs and display them together

--- Main.py
+++ Main.py
@@ -39,22 +39,65 @@ country_list = [
     'TWN', 'TZA', 'UGA', 'UKR', 'URY', 'USA', 'UZB', 'VCT', 'VEN', 'VNM', 'VUT', 'WSM', 'XKX', 'YEM', 'ZAF', 'ZMB', 'ZWE',
 ]
 
-def app():
-
-    # Creating an object of prediction service
-    pred = GeneralModel()
+class ResponseOutput:
+    def __init__(self):
+        self.responses = []
+        self.tasks = []
+        self.model = None
+        self.api_key = None
 
     # Using the streamlit cache
     @st.cache
-    def process_prompt(task, input, temperature, question=None):
-        return pred.model_prediction(task=task, input=input.strip(), api_key=api_key, temperature=temperature, question=question)
+    def process_prompt(self, task, input, temperature, question):
+        return self.model.model_prediction(
+            task=task,
+            input=input.strip(),
+            api_key=self.api_key,
+            temperature=temperature,
+            question=question,
+            )
+
+    def get_response(self, input, task, temperature, question):
+        response = self.process_prompt(
+            task, input, temperature, question)
+        
+        self.responses.append(response)
+        self.tasks.append(task)
+
+    def stream_responses(self):
+
+        for response, task in zip(self.responses, self.tasks):
+            st.markdown("***")
+            st.subheader(get_key(task_list, task))
+
+            if task == "embedding_task":
+                st.caption("Same country")
+                st.dataframe(response[0])
+                st.caption("Other countries")
+                st.dataframe(response[1])
+            elif task == "keyword_chunking_task":
+                for keyword in response:
+                    st.markdown("- " + keyword)
+            else:
+                st.markdown(response)
+
+    def clear_responses(self):
+        self.responses = []
+        self.tasks = []
+
+output = ResponseOutput()
+
+def app():
+
+    # Creating an object of prediction service
+    output.model = GeneralModel()
 
     # with st.sidebar:
     #     api_key = st.sidebar.text_input("APIkey", type="password")
 
-    api_key = st.secrets["OPENAI_KEY"]
+    output.api_key = st.secrets["OPENAI_KEY"]
 
-    if api_key:
+    if output.api_key:
         # MAIN
         st.title("Climate Policy Copilot 🤖")
         s_example = """Industrial products are vital to life in the UK, from the fabric of our buildings to the materials we use in our daily life. Without manufacturing industry, there would be no cars, no COVID-19 vaccine, no food or the packaging it goes in. UK industries combine high end technology and highly skilled workers with ingenuity to make products that are traded all over the world. Industry plays an essential role in society, contributing £170 billion to the overall economy. It is a high value area of employment, directly accounting for 9% of the UK’s GDP and providing 2.6 million direct jobs (ONS, Annual Business Survey, 2020) as well as over 5 million jobs across the value chain (UK in a Changing Europe, Manufacturing and Brexit, 2020). These businesses are disproportionately important to regions outside the South East, providing well paid jobs in areas such as the North West, Yorkshire and South Wales. This strategy covers the full range of UK industry sectors: metals and minerals, chemicals, food and drink, paper and pulp, ceramics, glass, oil refineries and less energy-intensive manufacturing1 . It also covers the new emerging industries, which will be the hallmarks of the net zero transition, including low carbon hydrogen and carbon capture, usage and storage.\n\nThis strategy comes at a time of substantial economic challenge and decreased ability of companies to invest. COVID-19 has resulted in a fall in production output, exports, and turnover growth across many of our industries. In the short term, industry has been able to access a range of cross economy government support, and in the longer term we are pursuing a green response to this crisis, including through the actions detailed in this strategy. At the same time, there are significant positive changes taking place in the global fight against climate change, which will support industry’s net zero transition. In November 2021, the UK will host the 26th UN Climate Change Conference of Parties (COP26), through which we are committed to reaching a constructive, negotiated outcome that drives forward collective climate action globally in line with the temperature goal of the Paris Agreement. The UK is also implementing and negotiating new free trade agreements following our exit from the EU, growing our decarbonisation export and collaboration opportunities in turn.\n\nSupporting the sectors to reach net zero carbon emissions by 2050 will provide new opportunities to level up the economy across all nations and regions of the country. Enabling investment in decarbonised technologies can drive job creation and new inward investment in the UK, and create new markets for our manufacturers. Decarbonisation also creates challenges for industry. Many essential low carbon5 technologies are in earlier stages of development, and not yet deployed regularly at a commercial level. Low carbon manufacturing will also be more expensive for some sectors, leading to an increase in their costs, and therefore risking a reduction in their competitiveness. This creates a risk of “carbon leakage” (Chapter 2), which could impact both our domestic and global climate goals. We need to work with industry to overcome these barriers in the coming decades. Any action taken will need to be consistent with our international obligations, both under the Paris Agreement and wider international trade rules. To meet net zero, our modelling shows industrial emissions will need to fall by at least 90% by 2050 (Chapter 4), equivalent to taking all the cars off the roads today. Any remaining emissions will need to be offset by separate methods, such as planting trees and capturing carbon from the air. All industrial sectors will need to act to meet this challenge. We need to transform how industry uses energy and makes products, and rethink the way consumers buy industrial products."""
@@ -68,6 +111,7 @@ def app():
 
         # SIDEBAR
         with st.sidebar:
+
             options = list(task_list.values())
             value = st.selectbox(
                 "Task", options, format_func=lambda x: get_key(task_list, x))
@@ -88,20 +132,12 @@ def app():
 
             if st.button("Run 🧠"):
                 with st.spinner(text="In progress"):
-                    response = process_prompt(
-                        value, input, temperature, question)
-                    if value == "embedding_task":
-                        st.markdown("Similar policies - Same country")
-                        st.dataframe(response[0])
-                        st.markdown("Similar policies - Other countries")
-                        st.dataframe(response[1])
-                    elif value == "keyword_chunking_task":
-                        for keyword in response:
-                            st.markdown("- " + keyword)
-                    else:
-                        st.markdown(response)
+                    output.get_response(input, value, temperature, question)
+            
+            output.stream_responses()
 
             if st.button("Clear"):
+                output.clear_responses()
                 st.empty()
 
     else:
