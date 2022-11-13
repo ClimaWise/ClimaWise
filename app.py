84027781ea9f0f86080abd4b9b6a3383bc9c287b
giuliobrugnaro <giuliobrugnaro@gmail.com>
2022-11-13 17:50:04 +0100

    emojis

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import Main
 import streamlit as st
 
-st.set_page_config(page_title="OpenAI Hackathon - Climate Policy Copilot", page_icon="random", layout="wide")
+st.set_page_config(page_title="OpenAI Hackathon - Climate Policy Copilot", page_icon="🌍", layout="wide")
 Main.app()
