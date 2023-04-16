49bd394e3a72718c785f5e2f8e925e09e424f205
Nicolò Trentacoste <n.trentacoste@nexum-ai.com>
2023-04-16 19:24:33 +0200

    Automatic download of the data

--- data/cpr_data_extraction.py
+++ data/cpr_data_extraction.py
@@ -2,18 +2,64 @@ import os
 import json
 import re
 import time
+import zipfile
 import openai
-import matplotlib.pyplot as plt
+import requests
 from bs4 import BeautifulSoup
-
-openai.api_key = "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"
 from model import get_embedding
 import pinecone
 
+openai.api_key = "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"
 pinecone.init(api_key="16a7a0ba-c9d6-44ce-9dd8-9a88d8643660", environment="us-east1-gcp")
+GITHUB_API = "https://api.github.com"
+REPO_OWNER = "climatepolicyradar"
+REPO_NAME = "open-data"
+TOKEN = "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"
+FILE_NAME = "commit_hash.txt"
 
 
-# Check if it's only summary
+def get_commit_hash():
+    url = f"{GITHUB_API}/repos/{REPO_OWNER}/{REPO_NAME}/commits"
+    headers = {"Authorization": f"token {TOKEN}"}
+    response = requests.get(url, headers=headers)
+    data = json.loads(response.text)
+
+    if response.status_code == 200:
+        return data[0]["sha"]
+    else:
+        print(f"Error: Unable to fetch commit hash. Status code {response.status_code}")
+        return None
+
+
+def read_commit_hash_from_file():
+    if os.path.exists(FILE_NAME):
+        with open(FILE_NAME, "r") as file:
+            return file.read().strip()
+    return None
+
+
+def write_commit_hash_to_file(commit_hash):
+    with open(FILE_NAME, "w") as file:
+        file.write(commit_hash)
+
+
+def download_and_extract_repo():
+    url = f"{GITHUB_API}/repos/{REPO_OWNER}/{REPO_NAME}/zipball"
+    headers = {"Authorization": f"token {TOKEN}"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        with open("repo.zip", "wb") as file:
+            file.write(response.content)
+
+        with zipfile.ZipFile("repo.zip", "r") as zip_ref:
+            zip_ref.extractall("repo")
+
+        os.remove("repo.zip")
+    else:
+        print(f"Error: Unable to download repo. Status code {response.status_code}")
+
+
 def is_fulltext(filename: str) -> bool:
     with open(filename, "r", encoding='utf-8') as file:
         data = json.load(file)
@@ -44,7 +90,7 @@ def get_fulltext_data(filename: str) -> tuple:
             else:
                 paragraphs[p_value] = text_no_tags.strip().replace("\n", " ").encode('ascii', 'ignore').decode()
 
-    return country_code, date, "fulltext_"+name, url, list(paragraphs.values())
+    return country_code, date, "fulltext_" + name, url, list(paragraphs.values())
 
 
 def get_description(filename: str) -> tuple:
@@ -59,8 +105,8 @@ def get_description(filename: str) -> tuple:
         text_no_tags = soup.get_text()
         description = text_no_tags.strip().replace("\n", " ").encode('ascii', 'ignore').decode()
 
-    return country_code, date, "description_"+name, url, description
-#TODO: Change the name with the paragraph+name
+    return country_code, date, "description_" + name, url, description
+
 
 def upsert_into_pinecone(country_code, date, name, url, text):
     index = pinecone.Index("climawise")
@@ -69,11 +115,14 @@ def upsert_into_pinecone(country_code, date, name, url, text):
             for elem in text:
                 time.sleep(1)
                 embedding = get_embedding(elem, engine="text-embedding-ada-002")
-                index.upsert([(name+"_"+str(text.index(elem)), embedding, {"name": name, "date": date, "country_code": country_code, "url": url, "source": "climate policy radar"})], namespace='policies')
+                index.upsert([(name + "_" + str(text.index(elem)), embedding,
+                               {"name": name, "date": date, "country_code": country_code, "url": url,
+                                "source": "climate policy radar"})], namespace='policies')
         else:
             time.sleep(1)
             embedding = get_embedding(text, engine="text-embedding-ada-002")
-            index.upsert([(name, embedding, {"name": name, "date": date, "country_code": country_code, "url": url, "source": "climate policy radar"})], namespace='policies')
+            index.upsert([(name, embedding, {"name": name, "date": date, "country_code": country_code, "url": url,
+                                             "source": "climate policy radar"})], namespace='policies')
     except Exception as E:
         print(E)
         time.sleep(30)
@@ -81,24 +130,38 @@ def upsert_into_pinecone(country_code, date, name, url, text):
             for elem in text:
                 time.sleep(1)
                 embedding = get_embedding(elem, engine="text-embedding-ada-002")
-                index.upsert([(name+"_"+str(text.index(elem)), embedding, {"name": name, "date": date, "country_code": country_code, "url": url, "source": "climate policy radar"})], namespace='policies')
+                index.upsert([(name + "_" + str(text.index(elem)), embedding,
+                               {"name": name, "date": date, "country_code": country_code, "url": url,
+                                "source": "climate policy radar"})], namespace='policies')
         else:
             time.sleep(1)
             embedding = get_embedding(text, engine="text-embedding-ada-002")
-            index.upsert([(name, embedding, {"name": name, "date": date, "country_code": country_code, "url": url, "source": "climate policy radar"})], namespace='policies')
+            index.upsert([(name, embedding, {"name": name, "date": date, "country_code": country_code, "url": url,
+                                             "source": "climate policy radar"})], namespace='policies')
 
 
-for dir_name in os.listdir('./data'):
-    if dir_name == 'ALB':
-        folder_path = os.path.join('./data', dir_name)
-        for file_name in os.listdir(folder_path):
-            if file_name.endswith('.json'):
-                filename_path = os.path.join(folder_path, file_name)
-                if is_fulltext(filename_path):
-                    country_code, date, name, url, full_text = get_fulltext_data(filename_path)
-                    upsert_into_pinecone(country_code, date, name, url, full_text)
-                    country_code, date, name, url, description = get_description(filename_path)
-                    upsert_into_pinecone(country_code, date, name, url, description)
-                else:
-                    country_code, date, name, url, description = get_description(filename_path)
-                    upsert_into_pinecone(country_code, date, name, url, description)
+if __name__ == "__main__":
+    latest_commit_hash = get_commit_hash()
+    stored_commit_hash = read_commit_hash_from_file()
+
+    if latest_commit_hash != stored_commit_hash:
+        print("Commit hash doesn't match. Downloading and updating the repo content.")
+        download_and_extract_repo()
+        write_commit_hash_to_file(latest_commit_hash)
+    else:
+        print("Commit hash matches. No need to update the repo content.")
+
+    # for dir_name in os.listdir('./data'):
+    #     if dir_name == 'ALB':
+    #         folder_path = os.path.join('./data', dir_name)
+    #         for file_name in os.listdir(folder_path):
+    #             if file_name.endswith('.json'):
+    #                 filename_path = os.path.join(folder_path, file_name)
+    #                 if is_fulltext(filename_path):
+    #                     country_code, date, name, url, full_text = get_fulltext_data(filename_path)
+    #                     upsert_into_pinecone(country_code, date, name, url, full_text)
+    #                     country_code, date, name, url, description = get_description(filename_path)
+    #                     upsert_into_pinecone(country_code, date, name, url, description)
+    #                 else:
+    #                     country_code, date, name, url, description = get_description(filename_path)
+    #                     upsert_into_pinecone(country_code, date, name, url, description)
